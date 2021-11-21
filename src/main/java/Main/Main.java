c6c94b3337c76f772dbfc6168f6617769620f5be
PabloA05 <pablo.cazon@alumnos.unc.edu.ar>
2021-11-21 16:49:44 -0300

    carga matrix y vector ini, cant hilos

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -2,10 +2,11 @@ package Main;
 
 import Monitor.*;
 
-import java.util.concurrent.Semaphore;
-
 public class Main {
     public static void main(String[] args) {
+
+        //todo implementar el numero de hilos
+        Operaciones.setCantidadHilos(10);
         String mji = "src/main/resources/inicial.csv";
         String I = "src/main/resources/incidencia.csv";
         RedDePetri redDePetri = new RedDePetri(mji, I);
