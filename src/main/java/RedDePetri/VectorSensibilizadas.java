0f2fb2e49098fb746b993ba56a6830ae6a357233
PabloA05 <pablo.cazon@alumnos.unc.edu.ar>
2021-11-22 15:47:26 -0300

    SensibilizadoConTiempo

--- src/main/java/RedDePetri/VectorSensibilizadas.java
+++ src/main/java/RedDePetri/VectorSensibilizadas.java
@@ -3,4 +3,24 @@ package RedDePetri;
 public class VectorSensibilizadas {
     private boolean esperando;
     private boolean ventana;
+    private SensibilizadoConTiempo sensibilizadoConTiempo;
+
+    private class SensibilizadoConTiempo{
+        private int alpha;
+        private int beta;
+        private long id;
+        private boolean flag;
+        private long startTime;
+        SensibilizadoConTiempo(int alpha, int beta){
+            this.alpha = alpha;
+            this.beta = beta;
+            this.flag = false;
+            this.startTime = 0;
+            this.id = -999999;
+        }
+    }
+    VectorSensibilizadas(){
+
+    }
+
 }
